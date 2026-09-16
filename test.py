4a98d75d2f3bc4c931d880993c59520da9ad72f8
unknown <xzt20030820@outlook.com>
2026-09-16 17:16:47 +0800

    inherit

--- test.py
+++ test.py
@@ -1587,3 +1587,63 @@ phone = Phone()
 # phone.__keep_single_core()
 phone.call_by_5g()
 # print(phone.__current_voltage)
+
+
+# 单继承
+
+class Phone:
+    IMEI = None
+    producer = "NIU"
+
+    def call_by_4g(self):
+        print("4g通话")
+
+
+
+class Phone2026(Phone):
+    face_id = "1000"
+
+    def call_by_5g(self):
+        print("2026新功能:5G通话")
+
+
+phone = Phone2026()
+print(phone.producer)
+
+phone.call_by_5g()
+phone.call_by_4g()
+
+# 多继承
+
+class NFCReader:
+    nfc_type = "第五代"
+    producer = "NZ"
+
+    def read_card(self):
+        print("NFC读卡")
+
+
+    def write_card(self):
+        print("NFC写卡")
+
+class RemoteControl:
+    rc_type = "红外遥控"
+
+    def control(self):
+        print("红外遥控开启了")
+
+
+
+class MyPhone(Phone, NFCReader, RemoteControl):
+    pass       # 不增添新功能
+
+
+phone = MyPhone()
+phone.call_by_4g()
+phone.read_card()
+phone.write_card()
+phone.control()
+
+print(phone.producer)    # 先继承的优先级高
+
+
