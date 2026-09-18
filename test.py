75930ef75c57737c1e921ef5f3d41157adc60098
unknown <xzt20030820@outlook.com>
2026-09-18 15:47:07 +0800

    Type hints

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
     这是一个用来测试的文件
 """
 
+import random
 import time
 
 # 打印一个整型字面量
@@ -1676,3 +1677,32 @@ class MyPhone(Phone):
 phone = MyPhone()
 phone.call_by_5g()
 print(phone.prodecer)
+
+
+# 类型注解
+class Student:
+    pass
+stu: Student = Student()
+
+# 基础容器类型注解
+# my_list: list = [1, 2, 3]
+# my_tuple: tuple = (1, 2, 3)
+# my_dict: dict = {"itheima":666}
+
+
+#容器类型详细注解
+my_list: list[int] = [1, 2, 3]
+my_dict: dict[str, int] = {"itheima": 666}
+my_tuple: tuple[int, str, bool] = (1, "itheima", True)
+
+
+#在注释中进行类型注解
+
+var_1 = random.randint(1, 10) # type: int
+var_2 = json.loads('{"name": "zhangsan"}') # type: dict[str, str]
+def func():
+    return 10
+var_3 = func()  # type: int
+#类型注解的限制
+var_4: int = "NIU"
+var_5: str = 123
